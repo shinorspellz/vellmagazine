c169ca49c66de63a9209f2beef7aed5f0a3bf5a5
MehmetCakir1 <mehmetcakir4227@gmail.com>
2023-02-04 20:35:17 +0300

    trending slider link updated

--- src/components/VellMagazine/SingleTrending.jsx
+++ src/components/VellMagazine/SingleTrending.jsx
@@ -7,7 +7,7 @@ import { articles } from '../../utils/vellMagazineData';
 const SingleTrending = ({item,index}) => {
   const {changeFormat}=useContext(VellMagazineContext)
   const navigate=useNavigate()
-    const {content,date,img,mainHeader,topics}=item;
+    const {content,date,img,mainHeader,topics,template,id}=item;
 
     const goToTopics=(e)=>{
       navigate(`/articletopics`,{state:e.target.innerText})
@@ -22,7 +22,7 @@ const SingleTrending = ({item,index}) => {
           onClick={(e)=>goToTopics(e)}
           >{topics[0]}</p>
            <h4 className='font-bold text-[14px] hover:underline cursor-pointer line-clamp-1 py-1 trending-title'
-            onClick={()=>navigate(`/article/${item.id}`)} >
+            onClick={()=>navigate(`/article/${id}/${template}`)} >
             {mainHeader.slice(0,20)} ...</h4>
         <p className='font-500 font-serif text-[14px] xl:py-4 line-clamp-3 hidden lg:block '>{content.startsWith("<p") ? content.slice(17,90).replace(/<.?p[^>]*>/g,"").replace(/<.?h.?[^>]*>/g,"") : content.slice(4,80).replace(/<.?p[^>]*>/g,"").replace(/<.?h.?[^>]*>/g,"")} </p>
         <p className='font-[500] text-[13px]'>{changeFormat(date)}</p>
