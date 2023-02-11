726a5227e304d55bedaa37c756c09e3025bfe080
Sau Ching Loke <loke.sau.ching@gmail.com>
2023-02-11 10:22:51 +0000

    refactor cookies

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -3,53 +3,75 @@ import { useContext } from "react";
 import CookieConsent from "react-cookie-consent";
 import { Link } from 'react-router-dom'
 import { VellMagazineContext } from "../context/VellMagazineContext";
-import { useCookieConsent } from "../context/CookieConsentContext";
+import { Cookies } from 'react-cookie';
 
-const Cookies = () => {
+
+const CookiesConsent = () => {
   const {theme,banner,setBanner}=useContext(VellMagazineContext)
-  const {consentChoice, acceptCookieConsent} = useCookieConsent()
-  const [visible, setVisible] = useState(false)
+  const [consent, setConsent] = useState(false);
+  const cookies = new Cookies();
 
   useEffect(() => {
-    setVisible(consentChoice === "undecided") 
+    const storedConsent = localStorage.getItem('cookieConsent');
+    if (storedConsent) {
+      setConsent(JSON.parse(storedConsent));
+    }
+  }, []);
 
-  }, [consentChoice])
+  useEffect(() => {
+    localStorage.setItem('cookieConsent', JSON.stringify(consent));
+  }, [consent]);
 
-  if(!visible) return null 
+  const handleConsent = () => {
+    setConsent(true);
+    cookies.set('cookieConsent', true, { path: '/' });
+  };
+  
+ 
+  
+
+  
 
 
   return (
     <div>
-      <CookieConsent 
-      debug={true}
-      location='bottom'
-      className="cookiesDiv"
-      visible={banner ? "show" :"hidden"}
-      //onAccept={()=>setBanner(false)}
-      onAccept={acceptCookieConsent}
-      style={theme==="light-theme" ? {
-        background:"#503C3B",
-        color:"#fff",
+      {!consent && (
+        <CookieConsent 
+        debug={true}
+        location='bottom'
+        className="cookiesDiv"
+        cookieName="cookieConsent"
+        //visible={banner ? "show" :"hidden"}
+        //onAccept={()=>setBanner(false)}
+        onAccept={handleConsent}
+
+        
+        style={theme==="light-theme" ? {
+          background:"#503C3B",
+          color:"#fff",
+        }:
+        {
+          background:"#ffd",
+          color:"#0a192f",
+        }
+      }
+      buttonStyle={theme==="light-theme" ? {
+        background:"#fff",
+        color:"#503C3B",
+        fontSize: "15px",
       }:
       {
-        background:"#ffd",
-        color:"#0a192f",
+        background:"#0a192f",
+        color:"#fff",
       }
     }
-    buttonStyle={theme==="light-theme" ? {
-      background:"#fff",
-      color:"#503C3B",
-      fontSize: "15px",
-    }:
-    {
-      background:"#0a192f",
-      color:"#fff",
-    }
-  }
-      // buttonStyle={{ color: "#503C3B", fontSize: "15px", background: 'white' }}
-      >This site uses cookies to enhance the user experience. See our <Link to="/privacypolicy" className="underline">privacy policy</Link> for more.</CookieConsent>
+        // buttonStyle={{ color: "#503C3B", fontSize: "15px", background: 'white' }}
+        >This site uses cookies to enhance the user experience. See our <Link to="/privacypolicy" className="underline">privacy policy</Link> for more.</CookieConsent>
+
+      )}
+      
     </div>
   )
 }
 
-export default Cookies 
+export default CookiesConsent
