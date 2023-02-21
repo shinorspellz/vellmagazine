4cc624b2835f49b29057ef349680330873a65974
MehmetCakir1 <mehmetcakir4227@gmail.com>
2023-02-22 02:27:59 +0300

    new articles

--- src/components/Articles/ArticleMainHeader.jsx
+++ src/components/Articles/ArticleMainHeader.jsx
@@ -32,7 +32,7 @@ const ArticleMainHeader = ({ item}) => {
               <h4 className={`${show && "hidden"} text-[#503C3B]  sm:pt-5 articleMainHeader `}>{item.mainHeader}</h4>
             )
           }
-        {/* <h3 className={`${show && "hidden"} absolute bottom-0 sm:bottom-2 left-[50%] translate-x-[-50%] text-[#503C3B] w-full text-xl sm:text-3xl  readmore`}> Read More...</h3> */}
+        <h3 className={`${show && "hidden"} absolute bottom-0 sm:bottom-2 left-[50%] translate-x-[-50%] text-[#503C3B] w-full text-xl sm:text-3xl  readmore`}> Read More...</h3>
       </div>
       </div>      
   )
